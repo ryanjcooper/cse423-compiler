b56840aa1ca485d38744491ada7740390b67d9f2
coopss <rcooper@nmt.edu>
2020-01-22 11:08:26 -0700

    Merge branch 'master' into fb_#13

---
+++ test/bubblesort2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+void swap(int *a, int *b) {
+        int tmp = *a;
+        *a = *b;
+        *b = tmp;
+}
+void bubblesort(int list[], int size) {
+        int i, j;
+
+        for(i = 0; i < size - 1; i++) {
+                for(j = 0; j < size-i-1; j++) {
+                        if(list[j] > list[j+1]) {
+                                swap(&list[j], &list[j+1]);
+                        }
+                }
+        }
+}
+void arrprint(int arr[], int size) {
+        for(int i = 0; i < size; i++) {
+                printf("%d ", arr[i]);
+        }
+        printf("\n");
+}
+int main(void) {
+        int list[10] = {0, 3, 5, 7, 9, 4, 2, 1, 6, 8};
+        int size = 10;
+
+        bubblesort(list, size);
+        arrprint(list, size);
+
+        return 0;
+}
---
+++ test/hello_world.c
@@ -0,0 +1,24 @@
+/**
+ * @author Brandon Fleming
+ * @date January 15th, 2020
+ * @file hello_world.c
+ *
+ * @detail Hello World program for testing a compiler
+ * @brief Prints a line
+ */
+
+#include <stdio.h>
+
+int main() {
+        //Here is a print statement
+        printf("Hello World\n");
+
+
+
+
+
+
+        return 0;
+}
+
+// Finished
