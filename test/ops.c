d1369e0e2faf3d2d431657435791d4c07f82c33f
RodianRomeo <Brandon.Fleming@student.nmt.edu>
2020-01-27 13:52:15 -0700

    Adding 2 more test cases

---
+++ test/ops.c
@@ -0,0 +1,28 @@
+/* This is a comment */
+int main()
+{
+        //Also a comment
+        int a, b, c;
+        a = 0;
+
+        c = a+1;
+        c = a-1;
+
+        b = 2;
+        a = 1;
+        c = a * b;
+        c = c * 2;
+        c = c / 2;
+
+        c*=2;
+        c/=2;
+
+        c++;
+        c--;
+        c-=1;
+
+        c = c << 1;
+        c = c >>1;
+
+        return c;
+}
