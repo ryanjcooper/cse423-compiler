a0fd717ba3cd89ab2e2518fda066caefbc88cc5c
tbrewer <terence.brewer@student.nmt.edu>
2020-02-06 16:37:14 -0700

    added parse tree

---
+++ src/main/java/edu/nmt/frontend/LockedStack.java
@@ -0,0 +1,52 @@
+package edu.nmt.frontend;
+
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class LockedStack {
+	
+	private ArrayDeque<ArrayList<Node>> stack;
+	
+	LockedStack() {
+		this.stack = new ArrayDeque<ArrayList<Node>>();
+	}
+	
+	public ArrayList<Node> pop() {
+		return this.stack.pop();
+	}
+	
+	public void push(ArrayList<Node> prevStack) {
+		this.stack.push(prevStack);
+	}
+	
+	public ArrayList<Node> peek() {
+		return this.stack.peek();
+	}
+	
+	public String peekString() {
+		String peek = "";
+		
+		for (Node node : this.stack.peek()) {
+			peek += " " + node;
+		}
+		
+		return peek.trim();
+	}
+	
+	public HashSet<String> peekFirsts(Grammar g) {
+		return g.computeFirsts(this.peekString());
+	}
+
+	public static void main(String[] args) throws IOException {
+		LockedStack ls = new LockedStack();
+		ls.push(new ArrayList<Node>());
+		ls.peek().add(new Node(new Token(null, "type", null, null)));
+		ls.peek().add(new Node(new Token(null, "identifier", null, null)));
+    	Grammar g = new Grammar("config/grammar.cfg");
+    	g.loadGrammar();		
+		System.out.println(ls.peekFirsts(g));
+	}
+
+}
