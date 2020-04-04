90d6fc4c677610d27c302291adaad1b6e96f0a45
tbrewer <terence.brewer@student.nmt.edu>
2020-04-04 12:02:49 -0600

    fixed apersand case

--- test/test.c
+++ test/test.c
@@ -1,14 +1,13 @@
-int foo(int i, int x);
-
 int main()
 {
-        int i = 0 + 7;
-
-        return foo(i + 1, 5);
-}
-
-int foo(int i, int x)
-{
-        i +=1;
-        return i;
+    int *i;
+    int j;
+    
+    j = 0;
+    
+    i = &j;
+    
+    *i = *i + 1;
+    
+    return *i;
 }
